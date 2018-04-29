3b30c2179871bbda06518feb9470100466223fc8
Jonathan <jonathanbcsouza@hotmail.com>
2018-04-30 01:11:44 +1200

    spinner working

--- app/src/main/java/com/tour/guide/TabAbout.java
+++ app/src/main/java/com/tour/guide/TabAbout.java
@@ -13,11 +13,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class TabAbout extends Fragment {
+public class TabAbout extends Fragment implements AdapterView.OnItemSelectedListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.about_layout, container, false);
+        final View rootView = inflater.inflate(R.layout.about_layout, container, false);
 
         Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner_sleeps);
         ArrayAdapter<CharSequence> adapterSpinner = ArrayAdapter.createFromResource(getActivity(),
@@ -25,7 +25,22 @@ public class TabAbout extends Fragment {
         adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapterSpinner);
 
+        spinner.setOnItemSelectedListener(this);
+
         return rootView;
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String getValue = (String) parent.getItemAtPosition(position);
+        Toast.makeText(getActivity(), getValue, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
+
 
